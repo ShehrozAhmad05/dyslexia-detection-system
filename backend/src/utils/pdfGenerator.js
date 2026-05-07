a0828d5a37dd112a0603c2859345b91a5610eaca
ShehrozAhmad05 <sheryidk123@gmail.com>
2026-05-07 17:49:13 +0500

    fix: update Puppeteer launch options and install additional dependencies in Docker setup

--- backend/src/utils/pdfGenerator.js
+++ backend/src/utils/pdfGenerator.js
@@ -10,7 +10,15 @@ async function generateAssessmentPDF(assessmentData) {
 
   const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
+    args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-gpu',
+    '--no-zygote',
+    '--single-process'
+  ]
   });
 
   try {
